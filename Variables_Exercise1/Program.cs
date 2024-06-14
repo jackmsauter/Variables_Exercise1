d97636f42222334b84c60da6cd1d6ead774a3aae
jacksauter <jackmsauter99@gmail.com>
2024-06-14 12:26:57 -0400

    initial commit

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Declare and Initialize Variables
+string name = "Oscar";
+int age = 31;
+char middleInitial = 'P';
+bool isover18 = true;
+double currentTemp = 77.9;
+decimal currentPrice = 22.99m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isover18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
